0167bb7fcd3fae3abc3ae7e5feacedb0848b1f4b
Mona Lisa <holmesmainsend@gmail.com>
2022-12-21 12:11:02 -0500

    testing typecasting

--- quickstart.java
+++ quickstart.java
@@ -3,5 +3,9 @@ class quickstart {
         // ONLY WORKS WITH SINGLE QUOTES
         char copyrightSymbol = '\u00C6';
         System.out.println(copyrightSymbol);
+
+        double number1 = 7.8;
+        int number2 = (int)number1;
+        System.out.println(number2);
     }
 }
