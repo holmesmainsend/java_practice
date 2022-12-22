ab754000d300df0700f6cb35c67a8b8dae795aaf
Mona Lisa <holmesmainsend@gmail.com>
2022-12-22 15:23:48 -0500

    successful user input test

--- userinput.java
+++ userinput.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class userinput {
     public static void main (String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is your last name?");
+        String name = scan.nextLine();
+        System.out.println(name);
+
+        scan.close();
     }
 }
