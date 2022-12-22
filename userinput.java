d0d5bb8daf1c008ebb759f0ca85aad426355be1b
Mona Lisa <holmesmainsend@gmail.com>
2022-12-22 15:38:47 -0500

    fixed program ending prematurely

--- userinput.java
+++ userinput.java
@@ -7,7 +7,18 @@ public class userinput {
         System.out.println("What is your last name?");
         String name = scan.nextLine();
         System.out.println(name);
-        System.out.printf("Hello %s. How are things? \n", name);
+
+        System.out.printf("Hello %s. How old are you? \n", name);
+        int age = scan.nextInt();
+
+        // cleaning up input buffer to prevent program's premature termination
+        scan.nextLine();
+
+        System.out.printf("%d is a good age. What is your favorite color? ", age);
+        String color = scan.nextLine();
+
+        // \n prevents "%" from appearing in the terminal window
+        System.out.printf("%s is a cool color. \n", color);
 
         scan.close();
     }
