edda56d100a9bbb1aaaea577f6d2e51da4d580b9
Mona Lisa <holmesmainsend@gmail.com>
2022-12-23 11:01:27 -0500

    testing different array creation methods

--- arrays.java
+++ arrays.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class arrays {
     public static void main (String args[]) {
-        
+        char[] vowels = new char[5];
+        vowels[0] = 'a';
+        vowels[1] = 'e';
+        vowels[2] = 'i';
+        vowels[3] = 'o';
+        vowels[4] = 'u';
+        System.out.println(Arrays.toString(vowels));
+
+        char[] lastLetters = {'x', 'y', 'z'};
+        System.out.println(Arrays.toString(lastLetters));
     }
 }
