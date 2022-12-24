e74ca2d539e1a48a7849a8b0455d857947d9b31c
Mona Lisa <holmesmainsend@gmail.com>
2022-12-24 12:29:49 -0500

    began testing arraylist

--- arraylists.java
+++ arraylists.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+
 public class arraylists {
     public static void main (String[] args) {
-        
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+
+        System.out.println(numbers.toString());
     }
 }
