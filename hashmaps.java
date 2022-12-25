d08e4b4dd05ae830852c81b9e5f2337841d7544b
Mona Lisa <holmesmainsend@gmail.com>
2022-12-25 14:02:53 -0500

    testing putIfAbsent method

--- hashmaps.java
+++ hashmaps.java
@@ -4,10 +4,14 @@ public class hashmaps {
     public static void main (String args[]) {
         HashMap<String, Integer> gradebook = new HashMap<String, Integer>();
 
-        gradebook.put("String", 87);
+        gradebook.put("Math", 87);
         gradebook.put("History", 91);
-        gradebook.put("English", 96);
+        gradebook.put("English", 76);
 
-        System.out.println(gradebook.get("English"));
+        gradebook.putIfAbsent("History", 46);
+        gradebook.putIfAbsent("Chemistry", 46);
+
+        // System.out.println(gradebook.get("English"));
+        System.out.println(gradebook.toString());
     }
 }
