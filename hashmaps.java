70c5c510a769669de0515a8a9e6027f038860ac4
Mona Lisa <holmesmainsend@gmail.com>
2022-12-25 14:18:50 -0500

    further practice with basic hashmap methods

--- hashmaps.java
+++ hashmaps.java
@@ -11,7 +11,24 @@ public class hashmaps {
         gradebook.putIfAbsent("History", 46);
         gradebook.putIfAbsent("Chemistry", 46);
 
+        gradebook.replace("Math", 61);
+
         // System.out.println(gradebook.get("English"));
+        // System.out.println(gradebook.get("Religion"));
+        // System.out.println(gradebook.getOrDefault("Religion", -1));
+        // System.out.println(gradebook.containsKey("Math"));
+        // System.out.println(gradebook.containsValue(21));
+
+        // gradebook.clear();
+
+        gradebook.forEach((subject, score) -> {
+            System.out.println(subject + ": " + (score * 10));
+        });
+        System.out.println(gradebook.toString());
+
+        gradebook.forEach((subject, score) -> {
+            gradebook.replace(subject, score * 10000);
+        });
         System.out.println(gradebook.toString());
     }
 }
