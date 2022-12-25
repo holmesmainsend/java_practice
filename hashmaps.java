51465b229c3ba76c1055fa5c0d7a01c4351805d0
Mona Lisa <holmesmainsend@gmail.com>
2022-12-25 14:00:30 -0500

    testing basics of hashmaps

--- hashmaps.java
+++ hashmaps.java
@@ -1,5 +1,13 @@
+import java.util.HashMap;
+
 public class hashmaps {
     public static void main (String args[]) {
-        
+        HashMap<String, Integer> gradebook = new HashMap<String, Integer>();
+
+        gradebook.put("String", 87);
+        gradebook.put("History", 91);
+        gradebook.put("English", 96);
+
+        System.out.println(gradebook.get("English"));
     }
 }
