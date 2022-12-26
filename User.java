091b02e32b1affeb8c2ea92f8bc2171062848c98
Mona Lisa <holmesmainsend@gmail.com>
2022-12-26 15:45:31 -0500

    added basic constructor and getters

--- User.java
+++ User.java
@@ -1,6 +1,19 @@
 public class User {
-    public String name;
-    public int birthYear;
+    private String name;
+    private int birthYear;
+
+    User(String name, int birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getBirthYear() {
+        return this.birthYear;
+    }
 
     public int currentAge() {
         return 2023 - birthYear;
--- oop.java
+++ oop.java
@@ -1,10 +1,12 @@
 public class oop {
     public static void main (String args[]) {
-        User youngerUser = new User();
+        User youngerUser = new User("Bobbyjoe", 1999);
 
-        youngerUser.name = "Bobbyjoe";
-        youngerUser.birthYear = 1999;
+        // youngerUser.name = "Bobbyjoe";
+        // youngerUser.birthYear = 1999;
 
-        System.out.printf("Hello %s, our records show that your birth year is %d making your current age approximately %d \n", youngerUser.name, youngerUser.birthYear, youngerUser.currentAge());
+        // System.out.printf("Hello %s, our records show that your birth year is %d making your current age approximately %d \n", youngerUser.name, youngerUser.birthYear, youngerUser.currentAge());
+
+        System.out.printf("Hello %s, our records show that your birth year is %d making your current age approximately %d.\n", youngerUser.getName(), youngerUser.getBirthYear(), youngerUser.currentAge());
     }
 }
