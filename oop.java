852fdd18227442a892e2b91d32836dd7ba1bd8d8
Mona Lisa <holmesmainsend@gmail.com>
2022-12-26 15:19:28 -0500

    successfully created user object

--- oop.java
+++ oop.java
@@ -1,5 +1,12 @@
+import java.time.LocalDate;
+
 public class oop {
     public static void main (String args[]) {
-        
+        User youngerUser = new User();
+
+        youngerUser.name = "Bobbyjoe";
+        youngerUser.birthDate = LocalDate.parse("1999-03-13");
+
+        System.out.printf("Hello %s, our records show your birthdate is %s \n", youngerUser.name, youngerUser.birthDate);
     }
 }
