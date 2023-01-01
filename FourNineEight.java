502e6e64d7664a7fcee14947a8e72ae09de06121
Mona Lisa <holmesmainsend@gmail.com>
2023-01-01 13:49:12 -0500

    testing basic traversal of 2D array

--- FourNineEight.java
+++ FourNineEight.java
@@ -1,14 +1,20 @@
 import java.util.Arrays;
 
-// For 1D array: System.out.println(Arrays.toString(arrayName));
-
 public class FourNineEight {
     public static void main(String[] args) {
         int[][] twoD = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         int m = twoD.length;
         int n = 3;
         int[] oneD = new int[m * n];
+        int z = 0;
 
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                oneD[z] = twoD[i][j];
+                z++;
+            }
+        }
         
+        System.out.println(Arrays.toString(oneD));
     }
 }
