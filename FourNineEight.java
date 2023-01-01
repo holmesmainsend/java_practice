1b76245d2e1a4efddb34e1de71f26060ad46f33c
Mona Lisa <holmesmainsend@gmail.com>
2023-01-01 13:32:52 -0500

    attempting new Leetcode problem

---
+++ FourNineEight.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+// For 1D array: System.out.println(Arrays.toString(arrayName));
+
+public class FourNineEight {
+    public static void main(String[] args) {
+        int[][] twoD = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int m = twoD.length;
+        int n = 3;
+        int[] oneD = new int[m * n];
+
+        
+    }
+}
