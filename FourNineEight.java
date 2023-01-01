1fa14507bac7846c497f6e5b10013018f36401e8
Mona Lisa <holmesmainsend@gmail.com>
2023-01-01 14:06:49 -0500

    looking for pattern in other test case

--- FourNineEight.java
+++ FourNineEight.java
@@ -2,11 +2,31 @@ import java.util.Arrays;
 
 public class FourNineEight {
     public static void main(String[] args) {
-        int[][] twoD = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        // int[][] twoD = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] twoD = {{1, 2}, {3, 4}};
         int m = twoD.length;
-        int n = 3;
+        // int n = 3;
+        int n = 2;
         int[] oneD = new int[m * n];
         int z = 0;
+
+        // oneD[z] = twoD[0][0];
+        // z++;
+        // oneD[z] = twoD[0][1];
+        // z++;
+        // oneD[z] = twoD[1][0];
+        // z++;
+        // oneD[z] = twoD[2][0];
+        // z++;
+        // oneD[z] = twoD[1][1];
+        // z++;
+        // oneD[z] = twoD[0][2];
+        // z++;
+        // oneD[z] = twoD[1][2];
+        // z++;
+        // oneD[z] = twoD[2][1];
+        // z++;
+        // oneD[z] = twoD[2][2];
 
         oneD[z] = twoD[0][0];
         z++;
@@ -14,17 +34,7 @@ public class FourNineEight {
         z++;
         oneD[z] = twoD[1][0];
         z++;
-        oneD[z] = twoD[2][0];
-        z++;
         oneD[z] = twoD[1][1];
-        z++;
-        oneD[z] = twoD[0][2];
-        z++;
-        oneD[z] = twoD[1][2];
-        z++;
-        oneD[z] = twoD[2][1];
-        z++;
-        oneD[z] = twoD[2][2];
         
         System.out.println(Arrays.toString(oneD));
     }
