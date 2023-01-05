7ad5a2091d93b0725d4f4f4cb7967b6a2e6d05aa
Mona Lisa <holmesmainsend@gmail.com>
2023-01-05 13:17:25 -0500

    updated explanatory note

--- LinkedListArrayList.java
+++ LinkedListArrayList.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class LinkedListArrayList {
     public static void main(String[] args) {
-        // TLDR: identical methods but very different operations behind the scenes
+        // TLDR: identical methods but very different operations behind the scenes. LinkedList faster at inserting/removing items, while ArrayList faster at searching for specific items
 
         LinkedList<String> fruitsLinkedList = new LinkedList<>();
         fruitsLinkedList.add("Banana");
