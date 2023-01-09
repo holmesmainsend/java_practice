3d3a73049aabdfd1c947bf52e4943f97cd20c31d
Mona Lisa <holmesmainsend@gmail.com>
2023-01-09 16:52:14 -0500

    reviewing recursion

---
+++ Recursion.java
@@ -0,0 +1,12 @@
+public class Recursion {
+    public static void main(String[] args) {
+        sayHi(3);
+    }
+
+    private static void sayHi(int count) {
+        System.out.println("Hi");
+        if(count > 1) {
+            sayHi(count - 1);
+        }
+    }
+}
