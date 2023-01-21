2ec21f7a6dd46d27e1ec49ba9c6276f85d1d9fec
Mona Lisa <holmesmainsend@gmail.com>
2023-01-21 12:06:37 -0500

    learned the basics of optionals

--- Animal.java
+++ Animal.java
@@ -1,12 +1,20 @@
 @VeryImportantCustomAnnotation
 public class Animal implements Printable {
+    // String name;
+    // int age;
+
+    // Animal(String name, int age) {
+    //     this.name = name;
+    //     this.age = age;
+    // }
+
     @ImportantString
     String taxonomicKingdom = "Animalia";
 
     @RunImmediately(times = 3)
     public void makeNoise() {
         System.out.println("Humanah humanah");
-    }
+    } 
 
     public void print() {
         System.out.println("I have satisfied the interface");
---
+++ Optionals.java
@@ -0,0 +1,19 @@
+import java.util.Optional;
+
+public class Optionals {
+    // public static void main(String[] args) {
+    //     Optional<Animal> optionalMyAnimal = findAnimalByName("Jeffrey");
+    //     if(optionalMyAnimal.isPresent()) {
+    //         System.out.println(optionalMyAnimal.get().age);
+    //     } else {
+    //         System.out.println("No such animal exists");
+    //     }
+        
+    // }
+
+    // private static Optional<Animal> findAnimalByName (String name) {
+    //     Animal animal = new Animal(name, 3);
+    //     // return Optional.ofNullable(null);
+    //     return Optional.ofNullable(animal);
+    // }
+}
